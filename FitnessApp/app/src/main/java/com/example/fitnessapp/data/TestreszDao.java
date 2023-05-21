0dce008cc1a7a2ca6787d11e084148755aeb081d
Adrián <adr200202@gmail.com>
2023-05-21 21:35:40 +0200

    Dávid munkáját megnéztem. Észrevettem, hogy egy interfészt rossz csomagba raktam, átmozgattam megfelelő helyre

--- FitnessApp/app/src/main/java/com/example/fitnessapp/data/TestreszDao.java
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/data/TestreszDao.java
@@ -1,7 +1,9 @@
-package com.example.fitnessapp.data.models;
+package com.example.fitnessapp.data;
 
 import androidx.room.Dao;
 import androidx.room.Query;
+
+import com.example.fitnessapp.data.models.Testresz;
 
 @Dao
 public interface TestreszDao {
